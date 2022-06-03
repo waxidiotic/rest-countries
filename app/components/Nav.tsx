1e0ce1578259788e5ff451b43acbdf2c70ebfa93
Alex Bussey <github@alexbussey.com>
2022-06-03 16:36:32 -0400

    Fix small responsive breakpoin

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -2,11 +2,13 @@ import { MdOutlineDarkMode } from "react-icons/md";
 
 export default function Nav() {
   return (
-    <div className="h-20 w-full flex justify-between bg-white px-20 items-center shadow-lg">
-      <div className="text-2xl font-extrabold">Where in the world?</div>
+    <div className="h-20 w-full flex justify-between bg-white px-4 sm:px-20 items-center shadow-lg">
+      <div className="sm:text-2xl text-sm font-extrabold">
+        Where in the world?
+      </div>
       <div className="font-semibold flex items-center gap-2">
-        <MdOutlineDarkMode className="h-5 w-5" />
-        <span>Dark Mode</span>
+        <MdOutlineDarkMode className="h-4 w-4 sm:h-5 sm:w-5" />
+        <span className="sm:text-base text-xs">Dark Mode</span>
       </div>
     </div>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -15,9 +15,9 @@ export default function Index() {
   const [searchTerm, setSearchTerm] = useState<string>();
 
   return (
-    <main className="px-20 py-10 max-w-[1441px]">
+    <main className="px-4 py-6 sm:px-20 sm:py-10 max-w-[1441px]">
       <div>
-        <div className="relative rounded-md shadow-md w-[480px] h-14 text-input-light">
+        <div className="relative rounded-md shadow-md w-full sm:w-[480px] h-14 text-input-light">
           <div className="absolute inset-y-0 left-0 pl-8 flex items-center pointer-events-none">
             <MdSearch className="fill-input-light h-7 w-7" />
           </div>
@@ -26,7 +26,7 @@ export default function Index() {
               type="text"
               name="search"
               id="search"
-              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-20 pr-12 sm:text-sm border-none rounded-md h-14"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pl-20 pr-12 text-sm border-none rounded-md h-14"
               placeholder="Search for a country..."
               onInput={event =>
                 setSearchTerm((event.target as HTMLInputElement).value)
@@ -36,7 +36,7 @@ export default function Index() {
         </div>
         <div>{/* TODO: Filter dropdown */}</div>
       </div>
-      <div className="mt-10 flex flex-wrap gap-[75px] w-full">
+      <div className="mt-10 flex flex-wrap gap-[75px] w-full sm:flex-row flex-col items-center sm:items-start">
         {searchTerm
           ? countries
               .filter(country =>
